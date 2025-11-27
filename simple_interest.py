3abf7f0d3dc8420b100ac90b9a870b45d0e7da09
Tanushreehurali <tanuhurali@gmail.com>
2025-11-27 21:11:31 +0530

    Update simple_interest.py

--- simple_interest.py
+++ simple_interest.py
@@ -1,14 +1,17 @@
 import sys
 
-# Check if enough arguments are passed
+
 if len(sys.argv) != 4:
     print("Usage: python simple_interest.py <principal> <rate> <time>")
     sys.exit(1)
+
 
 principal = float(sys.argv[1])
 rate = float(sys.argv[2])
 time = float(sys.argv[3])
 
+
 simple_interest = (principal * rate * time) / 100
 
-print("Simple Interest:", simple_interest)
+
+print("The Simple Interest is:", simple_interest)
